ccfb475f992363f1727013f453990fb2b62dc8ed
mshbeab <mshbeab@gmail.com>
2015-06-27 17:51:52 +0200

    added playerController,StarController scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+	public float Speed = 1f;
+
+
+	private Rigidbody2D player;
+
+
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+		GameObject star = null;
+		bool playerMoving = false; 
+
+
+		if (hit.collider != null) {
+			// Animate Star to show mouse hover
+			if (hit.collider.gameObject.tag == "Star") {
+				star = hit.collider.gameObject;
+				star.transform.localScale = new Vector3 (2f, 2f, 2f);
+			}
+			if(Input.GetMouseButtonDown(0)){
+				playerMoving = true ; 
+				StartCoroutine("MovePlayer",star);
+			}
+
+
+		} else {
+			// Rest the Stars to normal state
+//			GameObject[] Stars  = GameObject.FindGameObjectsWithTag("Star");
+//			foreach(GameObject s in Stars){
+//				s.transform.localScale = new Vector3(1f,1f,1f);
+//			}
+		}
+	}
+
+	IEnumerator MovePlayer(GameObject star){
+		float timeLerped = 0f;
+		while(timeLerped < 1.0) {
+			timeLerped += Time.deltaTime * Speed ;
+			transform.position = Vector2.Lerp(player.transform.position, star.transform.position, timeLerped );
+			yield return new WaitForEndOfFrame ();
+		}
+	}
+}
---
+++ Assets/Scripts/StarController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarController : MonoBehaviour {
+	public float minSize = 1f;
+	public float maxSize = 4f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log (gameObject.transform.localScale);
+		StartCoroutine("ChangeSize");
+	}
+
+
+	IEnumerator ChangeSize(){
+		if (gameObject.transform.localScale.x < maxSize) {
+			gameObject.transform.localScale = Vector3.Lerp(gameObject.transform.localScale,new Vector3(maxSize,maxSize,maxSize),Time.deltaTime);
+
+		} else {
+			gameObject.transform.localScale = Vector3.Lerp(gameObject.transform.localScale,new Vector3(minSize,minSize,minSize),Time.deltaTime);
+		}
+		yield return new WaitForEndOfFrame ();
+	}
+}
